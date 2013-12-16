47db75e8a2853c6e8e739143b1654db456489205
Sapotero Dominiko <egokok@gmail.com>
2013-12-16 13:18:42 +0400

    Signup support

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Twitter::Application.routes.draw do
+  get "users/new"
   resources :microposts
 
   resources :users
+  get '/signup',  to: 'users#new'
 
- get '/help',    to: 'static_pages#help'
- get '/about',   to: 'static_pages#about'
- get '/contact', to: 'static_pages#contact'
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 
 
   root 'static_pages#home'
